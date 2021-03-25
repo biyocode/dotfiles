56a72f00730137e00cf84f8b8b6ffc43f287eda6
kai-dg <haruspace.dev@gmail.com>
2021-03-26 00:21:48 +0900

    update

--- cpscript.sh
+++ cpscript.sh
@@ -1,40 +1,15 @@
 #!/usr/bin/env bash
 source scripts/globals
 source scripts/functions
-# Manjaro Architect 0
-echo -e "${C}Updating all dotfiles on Manjaro Architect 0:${NC}"
+echo -e "${C}Updating all dotfiles on Arch 0:${NC}"
 
 cp ~/.config/nvim/init.vim ./nvim/ 2> /dev/null
 status_check
 echo ": nvim - init.vim"
 
-cp ~/.config/nvim/coc-settings.json ./nvim/ 2> /dev/null
-status_check
-echo ": nvim - coc-settings.json"
-
-cp ~/.config/.compton.conf ./compton/ 2> /dev/null
-status_check
-echo ": compton - .compton.conf"
-
-cp ~/.config/konsolerc ./konsole/ 2> /dev/null
-status_check
-echo ": konsole - konsolerc"
-
-cp ~/.local/share/konsole/Main.profile ./konsole/ 2> /dev/null
-status_check
-echo ": konsole - Main.profile"
-
-cp ~/.local/share/konsole/konsole.css ./konsole/ 2> /dev/null
-status_check
-echo ": konsole - konsole.css"
-
 cp ~/.config/kitty/kitty.conf ./kitty/ 2> /dev/null
 status_check
 echo ": kitty - kitty.conf"
-
-cp ~/.i3/config ./i3/ 2> /dev/null
-status_check
-echo ": i3 - config"
 
 cp ~/.config/rofi/config.rasi ./rofi/ 2> /dev/null
 status_check
@@ -44,7 +19,7 @@ cp ~/.config/polybar/config ./polybar/ 2> /dev/null
 status_check
 echo ": polybar - config"
 
-cp ~/.bashrc ./bashrc/manjaro.bashrc 2>/dev/null
+cp ~/.bashrc ./bashrc/bashrc 2>/dev/null
 status_check
 echo ": bashrc - .bashrc"
 
