eb050b14ceb89209c9e68f4b1d917f20801b8915
kai-dg <haruspace.dev@gmail.com>
2023-02-18 22:02:19 +0900

    nvim:fix snippet setting

--- nvim/after/plugin/cmp.lua
+++ nvim/after/plugin/cmp.lua
@@ -26,15 +26,15 @@ cmp.setup({
     },
     snippet = {expand = function(args) luasnip.lsp_expand(args.body) end},
     mapping = cmp.mapping.preset.insert({
-        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.close(),
-        ['<CR>'] = cmp.mapping.confirm({
+        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.close(),
+        ["<CR>"] = cmp.mapping.confirm({
             behavior = cmp.ConfirmBehavior.Replace,
             select = true
         }),
-        ['<Tab>'] = cmp.mapping(function(fallback)
+        ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_next_item()
             elseif luasnip.expand_or_jumpable() then
@@ -42,8 +42,8 @@ cmp.setup({
             else
                 fallback()
             end
-        end, {'i', 's'}),
-        ['<S-Tab>'] = cmp.mapping(function(fallback)
+        end, {"i", "s"}),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_prev_item()
             elseif luasnip.jumpable(-1) then
@@ -51,11 +51,10 @@ cmp.setup({
             else
                 fallback()
             end
-        end, {'i', 's'})
+        end, {"i", "s"})
     }),
-    sources = cmp.config.sources({
-        {name = 'nvim_lsp'}, {name = 'buffer'}, {name = "luasnip"}
-    })
+    sources = cmp.config.sources({{name = "nvim_lsp"}, {name = "luasnip"}},
+                                 {{name = "buffer"}})
 })
 
 vim.cmd [[
