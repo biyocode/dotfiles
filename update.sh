c3ad0de78293b60859bb803f1433a1f8c11c3471
biyocode <haruspace.dev@gmail.com>
2023-12-29 00:40:09 +0900

    update:configs

--- update.sh
+++ update.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+cp ~/.config/rofi/config.rasi ~/00/dotfiles/rofi/config.rasi
 cp ~/.config/bspwm/bspwmrc ~/00/dotfiles/bspwm/bspwmrc
 cp ~/.config/picom/picom.conf ~/00/dotfiles/picom/picom.conf
 cp ~/.config/starship.toml ~/00/dotfiles/starship/starship.toml 
 cp ~/.config/kitty/kitty.conf ~/00/dotfiles/kitty/kitty.conf
 cp ~/.config/sxhkd/sxhkdrc ~/00/dotfiles/sxhkd/sxhkdrc
-rsync -av ~/.config/nvim ~/00/dotfiles/nvim
+cp ~/.bashrc ~/00/dotfiles/bashrc/bashrc
+rsync -av ~/.config/nvim ~/00/dotfiles
 
